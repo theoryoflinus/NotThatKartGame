923e89a435f823da1ee656c21568aaf52aeee75b
ImagineTactics <101190589+theoryoflinus@users.noreply.github.com>
2025-06-24 20:21:26 +0900

    init

--- Assets/Scripts/KartController.cs
+++ Assets/Scripts/KartController.cs
@@ -1,23 +1,53 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem; // Input System 사용
 
-public class KartController : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class SphereController : MonoBehaviour
 {
-    public float acceleration = 500f;
-    public float steering = 300f;
-
+    private PlayerInputActions.PlayerInputActions controls;
+    private Vector2 moveInput;
     private Rigidbody rb;
-    private void Start()
+
+    [SerializeField] private float moveForce = 10f;
+    [SerializeField] private float maxSpeed = 5f;
+
+    private void Awake()
     {
+        controls = new PlayerInputActions.PlayerInputActions();
+
+        // 이동 입력 처리
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
         rb = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void OnEnable()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        float steerInput = Input.GetAxis("Vertical");
+        controls.Enable();
+    }
 
-        rb.AddForce(transform.forward * moveInput * acceleration * Time.deltaTime);
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, steerInput * steering * Time.deltaTime, 0f));
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 forceDirection = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        // 현재 속도가 최대 속도보다 낮을 때만 힘을 추가
+        if (rb.linearVelocity.magnitude < maxSpeed)
+        {
+            rb.AddForce(forceDirection.normalized * moveForce, ForceMode.Force);
+        }
+
+        // 수평 속도 제한 (y축 제외)
+        Vector3 horizontalVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        if (horizontalVelocity.magnitude > maxSpeed)
+        {
+            Vector3 limitedVelocity = horizontalVelocity.normalized * maxSpeed;
+            rb.linearVelocity = new Vector3(limitedVelocity.x, rb.linearVelocity.y, limitedVelocity.z);
+        }
     }
 }
